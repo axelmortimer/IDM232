39bad71e95ed7255284d210be6245e2d44a8b3af
axelmortimer <axelmortimer@gmail.com>
2017-05-30 21:59:07 -0400

    Alpha

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include 'db_connection.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,21 +19,48 @@
 <body>
 	<header>
 		<div class="name">
-			<a href="index.html">Axel Mortimer</a>
+			<a href="index.php">Axel Mortimer</a>
 		</div>
 		<nav>
 			<div id="hamburger">
 				<img id="menu-icon" src="media/menu.svg" alt="menu-icon">
 			</div>
 			<ul>
-				<li><a href="about.html">About</a></li>
-				<li><a href="projects.html"><b>Portfolio</b></a></li>
-				<li><a href="playlists.html">Playlists</a></li>
-				<li><a href="contact.html">Contact</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#"><b>Portfolio</b></a></li>
+				<li><a href="#">Playlists</a></li>
+				<li><a href="#">Contact</a></li>
 			</ul>
 		</nav>
 	</header>
+
 	<main id="main-projects">
+
+		<?php
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo '<div class="project" id="' . $row['tag'] . '">';
+					echo '<h1>' . $row['projectTitle'] . '</h1>';
+					echo '<div class="project-images">';
+						echo '<picture>';
+							echo '<source media="(min-width: 50rem)" srcset="' . $row['courseImageSmall'] . '">';
+							echo '<img src="' . $row['courseImageLarge'] . '" alt="' . $row['projectTitle'] . '">';
+						echo '</picture>';
+					echo '</div>';
+					echo '<div class="project-info-bar">';
+						echo '<div class="project-info">';
+							echo '<p>' . $row['description'] . '</p>';
+						echo '</div>';
+						echo '<a href="' . rawurldecode($row['link']) . '">';
+							echo '<h3>VIEW</h3>';
+						echo '</a>';
+					echo '</div>';
+				echo '</div>';
+			}
+		?>
+
+	</main>
+
+	<!-- <main id="main-projects">
 		<div class="project" id="gvb-redesign">
 			<h1>GORILLA VS. BEAR REDESIGN</h1>
 			<div class="project-images">
@@ -83,7 +112,7 @@
 				</a>
 			</div>
 		</div>
-	</main>
+	</main> -->
 	<script src="js/main.js" type="text/javascript"></script>
 </body>
 </html>
